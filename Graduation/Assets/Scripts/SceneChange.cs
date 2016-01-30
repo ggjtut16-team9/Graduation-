93765b76ca176b6538419fc99e53ecd0249f0aeb
nishi <icemaunntenn@gmail.com>
2016-01-30 15:06:55 +0900

    追加

---
+++ Graduation/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+
+    public string m_NextScene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel(m_NextScene);
+        }
+	
+	}
+}
---
+++ Graduation/Assets/Scripts/TextList.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextList : MonoBehaviour {
+
+    public string[] m_Serif;
+    public string[] m_Shout;
+
+    public string GetSerif(int i)
+    {
+        return m_Serif[i];
+    }
+
+
+    public string GetShout(int i)
+    {
+        return m_Shout[i];
+    }
+}
