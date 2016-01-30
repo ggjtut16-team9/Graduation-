46059dd4a56d9c21bc142627decff47b308876ed
nishi <icemaunntenn@gmail.com>
2016-01-30 16:59:51 +0900

    タイトルのテキスト点滅

---
+++ Graduation/Assets/Scripts/FlashEffect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FlashEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Color c = Color.black;
+        c.a = Mathf.Sin(Time.frameCount * (0.08f)) * 0.5f;
+        GetComponent<Text>().color = c;
+    }
+}
